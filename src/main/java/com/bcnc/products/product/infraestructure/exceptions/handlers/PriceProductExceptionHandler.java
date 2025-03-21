d8e9288bfab0384365f72da7862fc5d0187a5864
elkin <elkin@gmail.com>
2025-03-21 14:08:41 +0100

    Fix Handler exception

--- src/main/java/com/bcnc/products/product/infraestructure/exceptions/handlers/PriceProductExceptionHandler.java
+++ src/main/java/com/bcnc/products/product/infraestructure/exceptions/handlers/PriceProductExceptionHandler.java
@@ -3,9 +3,11 @@ package com.bcnc.products.product.infraestructure.exceptions.handlers;
 import com.bcnc.products.product.infraestructure.exceptions.PriceProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.LocalDateTime;
 import java.util.Map;
@@ -14,6 +16,8 @@ import java.util.Map;
 public class PriceProductExceptionHandler {
     private static final String BAD_REQUEST_MESSAGE = "Parámetros inválidos o no existentes: ";
     private static final String INTERNAL_SERVER_ERROR_MESSAGE = "Ocurrió un error inesperado: ";
+    private static final String NOT_URL_FOUND_MESSAGE = "Url no encontrada: ";
+    private static final String REQUIRE_PARAMS_MESSAGE = "Parámetros incorrectos: ";
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<Object> handleBadRequest(MethodArgumentTypeMismatchException ex) {
@@ -23,6 +27,16 @@ public class PriceProductExceptionHandler {
     @ExceptionHandler(PriceProductNotFoundException.class)
     public ResponseEntity<Object> handleNotFound(PriceProductNotFoundException ex) {
         return buildResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<Object> handleNoHandlerFound(NoResourceFoundException ex) {
+        return buildResponse(HttpStatus.BAD_REQUEST, NOT_URL_FOUND_MESSAGE + ex.getResourcePath());
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Object> handleMethodNotSupported(MissingServletRequestParameterException ex) {
+        return buildResponse(HttpStatus.METHOD_NOT_ALLOWED, REQUIRE_PARAMS_MESSAGE + ex.getParameterName());
     }
 
     @ExceptionHandler(Exception.class)
